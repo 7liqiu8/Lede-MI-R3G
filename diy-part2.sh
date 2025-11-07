04f4fdf6ebf0df4b383ca109f80fa57c6c619f0b
vulcanford <fordvulcan@gmail.com>
2025-11-07 21:40:47 +0800

    Update banner URL to point to version 24.10

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,4 +25,4 @@ sed -i 's/LEDE/iStoreOS/g' package/base-files/files/etc/init.d/system
 sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/LEDE/iStoreOS/g' package/lean/default-settings/files/zzz-default-settings
 
-curl -o package/base-files/files/etc/banner https://raw.githubusercontent.com/istoreos/istoreos/refs/heads/istoreos-22.03/package/base-files/files/etc/banner
+curl -o package/base-files/files/etc/banner https://raw.githubusercontent.com/istoreos/istoreos/refs/heads/istoreos-24.10/package/base-files/files/etc/banner
